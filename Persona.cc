0dae6a9ed7e74909308f4a0022494c6d35b4603f
Francisco Gueto Gallego <frangue2105@gmail.com>
2018-12-03 18:17:18 +0100

    Añado Persona.cc

---
+++ Persona.cc
@@ -0,0 +1,45 @@
+#include "persona.h"
+#include <string>
+
+//Constructor de persona
+Persona::Persona(string dni, string nombre, string apellidos, int telefono, string direccion, string email){
+    dni_ = setDNI(dni);
+    nombre_ = nombre;
+    apellidos_ = apellidos;
+    telefono_ = setTelefono(telefono);
+    direccion_ = direccion;
+    email_ = email;
+}
+
+
+//Comprobacion de DNI
+bool Persona::setDNI(string nuevo_dni){
+    if (nuevo_dni.size()!=9){
+        return false;
+    }
+    if(nuevo_dni[8] != letraDNI(stoi(nuevo_dni))){
+        return false;
+    }
+    dni_ = nuevo_dni;
+    return true;
+}
+
+//Comprobar teléfono
+bool Persona::setTelefono(int nuevo_telefono){
+    if ((nuevo_telefono > 100000000) && (nuevo_telefono < 999999999)){
+        telefono_ = nuevo_telefono;
+        return true;
+    }
+    else{
+        return false;
+    }
+
+}
+
+//Return del nombre completo
+string Persona::getApellidosyNombre() const {
+    string nombrecompleto;
+    nombrecompleto = nombre_ + " " + apellidos_;
+    return nombrecompleto;
+
+}
