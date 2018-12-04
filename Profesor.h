b3f354c98f5c9e5e94a40eb7c02cc1039817e161
Sergio <sergio.alhama.11@gmail.com>
2018-12-04 12:45:12 +0100

    Añadimos clase Profesor

---
+++ Profesor.h
@@ -0,0 +1,15 @@
+#ifndef _PROFESOR_H_
+#define _PROFESOR_H_
+
+#include<string>
+using namespace std;
+
+class Profesor{
+    private:
+        string dni_;
+        string nombre_;
+        string apellidos_;
+        string fechanacimiento_;
+        int telefono_;
+        string email_;
+        string direccion_;
